9fedb861cffc7aad5a61c186796f70dade75a529
ssk090 <shivanandasai.38@gmail.com>
2024-09-19 23:50:51 +0530

    Step 6: Sets

--- utils/keys.ts
+++ utils/keys.ts
@@ -3,3 +3,6 @@ export const getKeyName = (...args: string[]) => {
 };
 
 export const restaurantKeyById = (id: string) => getKeyName("restaurants", id);
+export const reviewKeyById = (id: string) => getKeyName("reviews", id);
+export const reviewDetailsKeyById = (id: string) =>
+  getKeyName("review_details", id);
